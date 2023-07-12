179ffbeb07755f3647822723e5511707d6ff1a4a
isharghura <ishar.ghuras@gmail.com>
2023-07-12 10:58:09 -0400

    added several more functions to chessBot.py

--- chessBot.py
+++ chessBot.py
@@ -1,4 +1,5 @@
 import chess as ch
+import random as rd
 
 class Bot:
 
@@ -6,6 +7,46 @@ class Bot:
         self.board=board
         self.maxDepth=maxDepth
         self.color=color
+    
+    def bestMove(self):
+        return self.bot(None, 1)
+
+    def evalFunction(self):
+        compt = 0
+        for i in range(64):
+            compt+=self.squareResPoints(ch.SQUARES[i])
+        compt+=self.mateOpportunity()+self.opening()+0.001*rd.random()
+
+    def opening(self):
+        if(self.board.fullmove_number<10):
+            if(self.board.turn==self.color):
+                return 1/30 * self.board.legal_moves.count()
+            else:
+                return 1/30 * self.board.legal_moves.count()
+        else:
+            return 0
+
+    def mateOpportunity(self):
+        if(self.board.legal_moves.count()==0):
+            if(self.board.turn==self.color):
+                return -999
+            else:
+                return 999
+        else:
+            return 0
+        
+    def squareResPoints(self, square):
+        pieceValue=0
+        if(self.board.piece_type_at(square)==ch.PAWN):
+            pieceValue=1
+        if(self.board.piece_type_at(square)==ch.KNIGHT):
+            pieceValue=3.2
+        if(self.board.piece_type_at(square)==ch.BISHOP):
+            pieceValue=3.33
+        if(self.board.piece_type_at(square)==ch.ROOK):
+            pieceValue=5.1
+        if(self.board.piece_type_at(square)==ch.QUEEN):
+            pieceValue=8.8
 
     def bot(self, candidate, depth):
         if(depth==self.maxDepth or self.board.legal_moves.count()==0):
@@ -37,3 +78,14 @@ class Bot:
                 if(candidate!=None and value < candidate and depth%2==0):
                     self.board.pop()
                     break
+
+                elif(candidate!=None and value > candidate and depth%2!=0):
+                    self.board.pop()
+                    break
+
+                self.board.pop()
+
+        if(depth>1):
+            return candidate
+        else:
+            return move
